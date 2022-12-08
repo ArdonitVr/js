de6265ff5a5125deb5e107357292ce639053824c
ArdonitVr <ardonit.vrella1@gmail.com>
2022-12-08 23:31:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let subscribed = false
+
+let loggedIn = true
+
+if (subscribed === true) {
+    console.log('Show the video')
+}
+
+else if (loggedIn === true) {
+    console.log('Become a Pro user')
+}
+
+else {
+    console.log('Hide the Video')
+}
